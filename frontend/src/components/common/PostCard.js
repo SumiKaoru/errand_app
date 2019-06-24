37851d12fcd4a808d10142c5dd92aebcaa964600
Dotsung <andy395023@gmail.com>
2019-06-24 14:59:06 +0900

    PostCard Component

--- frontend/src/components/common/PostCard.js
+++ frontend/src/components/common/PostCard.js
@@ -4,10 +4,13 @@ import styled from 'styled-components';
 
 const WhiteBox = styled.div`
     background-color: white;
+    width: 100%;
+    height: 200px;
 `
 
 const Wrapper = styled.div`
-    margin-top: 50px;
+    padding: 20px;
+    display: flex;
 `
 
 class PostCard extends Component {
@@ -15,7 +18,9 @@ class PostCard extends Component {
     render() {
         return (
             <WhiteBox>
-                asdf
+                <Wrapper>
+                    asdf
+                </Wrapper>
             </WhiteBox>
         );
     }
--- frontend/src/containers/common/PostList.js
+++ frontend/src/containers/common/PostList.js
@@ -5,8 +5,23 @@ import styled from 'styled-components';
 import PostCard from 'components/common/PostCard';
 
 const Wrapper = styled.div`
-    margin-top: 50px;
-    padding: 30px;
+    margin: 0 auto;
+    margin-top: 70px;
+    width: 40%;
+    @media (max-width: 1920px) {
+        width: 40%;
+    }
+    @media (max-width: 1600px) {
+        width: 50%
+    }
+    @media (max-width: 1200px) {
+        width: 70%
+    }
+    @media (max-width: 700px) {
+        width: 95%;
+    }
+    display: flex;
+    flex-direction: column;
 `
 
 class PostList extends Component {
